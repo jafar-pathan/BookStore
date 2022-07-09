25dcf9625640409c48aea89aef6646951ce09b16
Jafar Pathan <65374935+jafar-pathan@users.noreply.github.com>
2022-07-09 17:03:30 +0530

    Add files via upload

---
+++ BackEnd/BookStore.Api/Controllers/BookStoreController.cs
@@ -0,0 +1,36 @@
+﻿
+using BookStore.Models.Models;
+using BookStore.Models.ViewModels;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/BookStore")]
+    [ApiController]
+    public class BookStoreController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginModel model)
+        {
+            User user = _repository.Login(model);
+            if (user == null)
+                return NotFound();
+
+            UserModel userModel = new UserModel(user);
+            return Ok(userModel);
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(RegisterModel model)
+        {
+            User user = _repository.Register(model);
+            UserModel userModel = new UserModel(user);
+            return Ok(userModel);
+        }
+    }
+}
---
+++ BackEnd/BookStore.Api/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿
+using System.Linq;
+using BookStore.Models.Models;
+using BookStore.Models.ViewModels;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/Category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        CategoryRepository _categoryRepository = new CategoryRepository();
+        [Route("list")]
+        [HttpGet]
+        public IActionResult GetCategories(int pageIndex,int pageSize=10,string keyword="")
+        {
+            var categories=_categoryRepository.GetCategories(pageIndex, pageSize, keyword);
+            ListResponse<CategoryModel> listResponse = new ListResponse<CategoryModel>()
+            {
+                Results = categories.Results.Select(c => new CategoryModel(c)),
+                TotalRecords = categories.TotalRecords,
+            }; 
+            return Ok(listResponse);
+        }
+        [Route("{id}")]
+        [HttpGet]
+
+      public IActionResult GetCategory(int id)
+        {
+            var category = _categoryRepository.GetCategory(id);
+            CategoryModel categoryModel = new CategoryModel(category);
+            return Ok(categoryModel);
+        }
+        [Route("add")]
+        [HttpPost]
+
+        public IActionResult AddCategory(CategoryModel model)
+        {
+            Category category = new Category()
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+            var response = _categoryRepository.AddCategory(category);
+            CategoryModel categoryModel = new CategoryModel(response);
+            return Ok(categoryModel);
+        }
+        [Route("update")]
+        [HttpPut]
+        public IActionResult UpdateCategory(CategoryModel model)
+        {
+            Category category = new Category()
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+            var response = _categoryRepository.UpdateCategory(category);
+            CategoryModel categoryModel = new CategoryModel(response);
+            return Ok(categoryModel);
+        }
+        [Route("delete/{id}")]
+        [HttpDelete]
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var response = _categoryRepository.DeleteCategory(id);
+            return Ok(response);
+        }
+
+
+
+    }
+}
